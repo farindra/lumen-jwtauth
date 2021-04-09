d837f5547ea95574483dc0369ba12bebd70e13a5
Farindra Eka Putra <farindra.project@gmail.com>
2021-04-09 18:33:29 +0700

    setup cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,9 +75,9 @@ $app->configure('jwt');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class,
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
